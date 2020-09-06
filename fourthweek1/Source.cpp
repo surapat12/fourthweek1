adeae3d8c80b4f77e40ff8d39bb880eedf480eda
Surapat Wongprapaipak <69204357+surapat12@users.noreply.github.com>
2020-09-06 12:51:09 +0700

    Update Source.cpp

--- fourthweek1/Source.cpp
+++ fourthweek1/Source.cpp
@@ -2,30 +2,42 @@
 #include<conio.h>
 int main()
 {
-	int a[9][9], i, j,x;
-	for (i = 1; i <= 9; i++)
+	int a[20][20], i, j, x,m;
+	printf("Enter the number(No more than 20) : ");
+	scanf_s("%d", &m);
+	if (m > 20)
 	{
-		for (j = 1; j <= 9; j++)
+		printf("ERROR");
+	}
+	for (i = 1; i <= m; i++)
+	{
+		for (j = 1; j <= m; j++)
 		{
 			x = i - 1;
 			a[x][j - 1] = i * j;
 
 		}
 	}
-	printf("*    |\t1\t2\t3\t4\t5\t6\t7\t8\t9\n");
-	printf("---------------------------------------------------------------------------\n");
-	for (i = 0; i <9; i++)
+	printf("*    |");
+	int n = 1;
+	for (n = 1; n <= m; n++)
 	{
-		printf("%d    |\t", i+1);
-		for (j = 0; j <9; j++)
+		printf("\t%d", n);
+	}
+	printf("\n");
+	printf("----------------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+	for (i = 0; i < m; i++)
+	{
+		printf("%d    |\t", i + 1);
+		for (j = 0; j < m; j++)
 		{
 			printf("%d\t", a[i][j]);
-			
+
 		}
 		printf("\n");
 
 	}
-	
+
 
 
 	return 0;
